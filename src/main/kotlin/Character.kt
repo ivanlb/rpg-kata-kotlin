48dd495c356ca59c898b73985fd1f788daf18a8d
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2020-07-06 12:28:56 +0200

    Set default values on character

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,3 +1,5 @@
 data class Character(
-    val health: Int = 1000
+    val health: Int = 1000,
+    val level: Int = 1,
+    val alive: Boolean = true
 )
--- src/test/kotlin/CharacterTest.kt
+++ src/test/kotlin/CharacterTest.kt
@@ -3,9 +3,11 @@ import org.junit.jupiter.api.Test
 
 class CharacterTest {
     @Test
-    fun `when created has health 1000`() {
+    fun `when created has default values`() {
         val character = Character()
 
         assertThat(character.health).isEqualTo(1000)
+        assertThat(character.level).isEqualTo(1)
+        assertThat(character.alive).isTrue()
     }
 }
