9a34183d314e76dcd7f974f6951660a780cc2e41
Ivan Lombardi Borgia <ivan.lombardiborgia@gmail.com>
2020-09-22 11:53:28 +0200

    iteration 1.3

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,21 +1,27 @@
 import kotlin.math.max
+import kotlin.math.min
 
 class Character(
-    var health: Int = INITIAL_HEALTH,
+    var health: Int = MAX_HEALTH,
     val level: Int = 1
 ) {
     fun alive() = health > MIN_HEALTH
 
-    fun dealDamage(target: Character, damage: Int) {
-        target.sufferAttack(damage)
-    }
+    fun dealDamage(target: Character, damage: Int) = target.sufferAttack(damage)
+
+    fun heal(target: Character, health: Int) = target.receiveCare(health)
 
     private fun sufferAttack(damage: Int) {
         health = max(health - damage, MIN_HEALTH)
     }
 
-    companion object{
-        private const val INITIAL_HEALTH = 1000
+    private fun receiveCare(healing: Int) {
+        if (alive())
+            health = min(health + healing, MAX_HEALTH)
+    }
+
+    companion object {
+        private const val MAX_HEALTH = 1000
         private const val MIN_HEALTH = 0
     }
 }
--- src/test/kotlin/CharacterTest.kt
+++ src/test/kotlin/CharacterTest.kt
@@ -6,33 +6,55 @@ class CharacterTest {
     fun `when created has default values`() {
         val character = Character()
 
-        assertThat(character.health).isEqualTo(INITIAL_HEALTH)
+        assertThat(character.health).isEqualTo(1000)
         assertThat(character.level).isEqualTo(1)
         assertThat(character.alive()).isTrue()
     }
 
     @Test
     fun `characters can deal damage to characters`() {
-        val character = Character()
         val target = Character()
 
-        character.dealDamage(target, damage = 300)
+        Character().dealDamage(target, damage = 300)
 
         assertThat(target.health).isEqualTo(700)
     }
 
     @Test
     fun `when damage received exceeds current health, health becomes 0 and the character dies`() {
-        val character = Character()
         val target = Character()
 
-        character.dealDamage(target, damage = 1100)
+        Character().dealDamage(target, damage = 1100)
 
         assertThat(target.health).isEqualTo(0)
         assertThat(target.alive()).isFalse()
     }
 
-    companion object {
-        private const val INITIAL_HEALTH = 1000
+    @Test
+    fun `a character can heal a character`() {
+        val target = Character(health = 800)
+
+        Character().heal(target, 100)
+
+        assertThat(target.health).isEqualTo(900)
+    }
+
+    @Test
+    fun `healing cannot raise health above 1000`() {
+        val target = Character(health = 800)
+
+        Character().heal(target, 300)
+
+        assertThat(target.health).isEqualTo(1000)
+    }
+
+    @Test
+    fun `dead characters cannot be healed`() {
+        val deadCharacter = Character(health = 0)
+
+        Character().heal(deadCharacter, 300)
+
+        assertThat(deadCharacter.health).isEqualTo(0)
+        assertThat(deadCharacter.alive()).isFalse()
     }
 }
