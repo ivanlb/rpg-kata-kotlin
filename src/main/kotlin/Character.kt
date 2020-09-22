7d8a2c874aa7c17b5f231e5f99368bfaa5cbfedd
Ivan Lombardi Borgia <ivan.lombardiborgia@gmail.com>
2020-09-22 11:37:17 +0200

    iteration 1.1 and 1.2

--- src/main/kotlin/Character.kt
+++ src/main/kotlin/Character.kt
@@ -1,5 +1,21 @@
-data class Character(
-    val health: Int = 1000,
-    val level: Int = 1,
-    val alive: Boolean = true
-)
+import kotlin.math.max
+
+class Character(
+    var health: Int = INITIAL_HEALTH,
+    val level: Int = 1
+) {
+    fun alive() = health > MIN_HEALTH
+
+    fun dealDamage(target: Character, damage: Int) {
+        target.sufferAttack(damage)
+    }
+
+    private fun sufferAttack(damage: Int) {
+        health = max(health - damage, MIN_HEALTH)
+    }
+
+    companion object{
+        private const val INITIAL_HEALTH = 1000
+        private const val MIN_HEALTH = 0
+    }
+}
--- src/test/kotlin/CharacterTest.kt
+++ src/test/kotlin/CharacterTest.kt
@@ -6,8 +6,33 @@ class CharacterTest {
     fun `when created has default values`() {
         val character = Character()
 
-        assertThat(character.health).isEqualTo(1000)
+        assertThat(character.health).isEqualTo(INITIAL_HEALTH)
         assertThat(character.level).isEqualTo(1)
-        assertThat(character.alive).isTrue()
+        assertThat(character.alive()).isTrue()
+    }
+
+    @Test
+    fun `characters can deal damage to characters`() {
+        val character = Character()
+        val target = Character()
+
+        character.dealDamage(target, damage = 300)
+
+        assertThat(target.health).isEqualTo(700)
+    }
+
+    @Test
+    fun `when damage received exceeds current health, health becomes 0 and the character dies`() {
+        val character = Character()
+        val target = Character()
+
+        character.dealDamage(target, damage = 1100)
+
+        assertThat(target.health).isEqualTo(0)
+        assertThat(target.alive()).isFalse()
+    }
+
+    companion object {
+        private const val INITIAL_HEALTH = 1000
     }
 }
